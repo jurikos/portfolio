aca438043fe4b519a06b070443df0f8d712c7b7b
Juri Kostjunin <juri.kostjunin@gmail.com>
2019-03-19 19:57:32 +0200

    Added content.json, Loading

---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Loading = () => {
+  return <div className="c-loading" />
+};
+
+export default Loading;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
 
+import Loading from './Loading';
 import TechStack from './main/TechStack';
 import ContactMe from './main/ContactMe';
 
 class Main extends Component {
+  handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: null,
+      techStack: null,
+      contactMe: null
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://jurikos.github.io/portfolio/content.json';
+    const cached = sessionStorage.getItem(url);
+
+    if (!cached) {
+      fetch(url)
+        .then(this.handleErrors)
+        .then(res => res.json())
+        .then(json => {
+          const stringifiedJson = JSON.stringify(json)
+
+          sessionStorage.setItem(url, stringifiedJson);
+
+          this.setState({
+            content: JSON.parse(stringifiedJson)
+          });
+
+          return json;
+        })
+        .catch(error => console.error(error));
+    } else {
+      this.setState({
+        content: JSON.parse(cached)
+      });
+    }
+  }
+
   render() {
+    const content = this.state.content ? this.state.content.summary : null;
+
     return (
       <main className="l-main" data-role="main">
         <div className="c-content">
           <h1 className="c-content__heading v-animation v-animation--slide-in-down">Frontend Developer</h1>
-          <article className="c-content__txt v-animation v-animation--fade-in-up">
-            <p>
-              Senior <strong>Frontend Web &amp; Mobile</strong> developer with a strong focus on
-              <strong>performance optimisation</strong>, <strong>clean code</strong> and passion to <strong>UX</strong>.
-              I have 10 Years of professional experience developing high-end <strong>ecommerce</strong> &amp; <strong>
-              corporate websites</strong>, cross-platform <strong>mobile apps</strong>.
-            </p>
-          </article>
+          {content ? <article className="c-content__txt v-animation v-animation--fade-in-up"
+                              dangerouslySetInnerHTML={{ __html: content }} /> : <Loading />}
           <div className="c-content__cta">
             <TechStack />
             <ContactMe />
