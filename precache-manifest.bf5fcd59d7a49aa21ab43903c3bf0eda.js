f5d839ed7409b673e6006addeaaaa18fd4f91dbd
Juri Kostjunin <juri.kostjunin@gmail.com>
2019-04-05 13:52:23 +0300

    Updates

---
+++ precache-manifest.bf5fcd59d7a49aa21ab43903c3bf0eda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fad3cc96c0df01ceab07",
+    "url": "/portfolio/static/js/main.a1c63fa0.chunk.js"
+  },
+  {
+    "revision": "95493f2e5f0167321aa8",
+    "url": "/portfolio/static/js/2.742f0fac.chunk.js"
+  },
+  {
+    "revision": "fad3cc96c0df01ceab07",
+    "url": "/portfolio/static/css/main.20aa43ef.chunk.css"
+  },
+  {
+    "revision": "6a6eb16ce1c15678a294dd953e9fbdf2",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c518f11a24b2fb354e2615043b4fc982.js"
+  "/portfolio/precache-manifest.bf5fcd59d7a49aa21ab43903c3bf0eda.js"
 );
 
 workbox.clientsClaim();
