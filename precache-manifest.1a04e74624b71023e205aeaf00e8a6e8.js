363701f637fefd1b9739d935ae05b96cb2895bb5
Juri Kostjunin <juri.kostjunin@gmail.com>
2019-08-05 11:58:11 +0300

    Updates

---
+++ precache-manifest.1a04e74624b71023e205aeaf00e8a6e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeaa77913a0464082b2038a5e1dcc96f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "96ba43e1e8b96f5b883a",
+    "url": "/portfolio/static/css/main.877cddec.chunk.css"
+  },
+  {
+    "revision": "b1dae739d5f56456578e",
+    "url": "/portfolio/static/js/2.53afacdb.chunk.js"
+  },
+  {
+    "revision": "96ba43e1e8b96f5b883a",
+    "url": "/portfolio/static/js/main.99037020.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
