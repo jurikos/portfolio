948f4a19049bdb99d8c7e7f3be3d47ff3c83d6cd
Juri Kostjunin <juri.kostjunin@gmail.com>
2020-05-08 23:59:40 +0300

    Updates

---
+++ precache-manifest.194142b9359ef52f2430b73e5e38b3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb6b1b61f2efae723ed496aa9d553bf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bcf2a181b986f4e22cd8",
+    "url": "/portfolio/static/js/2.d68c6e24.chunk.js"
+  },
+  {
+    "revision": "a0ef5c81e0ee77044ca5daf1c34b9fbb",
+    "url": "/portfolio/static/js/2.d68c6e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e38c336b897e6e4953",
+    "url": "/portfolio/static/js/main.c0230cfe.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.59d5eeffb108aaa9cee68374185cac58.js"
+  "/portfolio/precache-manifest.194142b9359ef52f2430b73e5e38b3b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
